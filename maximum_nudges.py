652a8e8c3f72b6f042c8caa895a621dc8b09a52f
Derkjan <derkjanriesthuis@gmail.com>
2017-09-08 00:04:04 +0200

    add maximization method without taking conditional explicitly

--- maximum_nudges.py
+++ maximum_nudges.py
@@ -133,7 +133,6 @@ def find_minimum_subset(weights, scores, total_size):
                                selected_weights[-1])
     return selected_indices, selected_weights
 
-
 def find_maximum_local_nudge(input_dist, cond_output, nudge_size):
     """
 
@@ -249,6 +248,130 @@ def find_maximum_local_nudge(input_dist, cond_output, nudge_size):
 
     return max_impact
 
+def find_maximum_local_nudge_without_conditional(input_dist, cond_output, nudge_size):
+    """
+
+    Parameters:
+    ----------
+    input_dist: a numpy array
+        Representing a probability distribution. The nudged variable
+        should be the last variable in input_dist
+    cond_output: a numpy array
+        The dimensions should be have be the total size of the input 
+        distribution times the amount of output states
+    nudge_size: a number
+
+    Returns: a number
+        The maximum nudge nudge_size
+
+    """
+    number_of_nudge_states = input_dist.shape[-1]
+    number_of_input_states = input_dist.flatten().shape[0]
+    number_of_input_variables = len(input_dist.shape)
+    number_of_output_states = cond_output.shape[-1]
+    #print("number of input variables {}".format(number_of_input_variables))
+    
+    other_input_dist = ProbabilityArray(input_dist).marginalize(
+        set(range(number_of_input_variables-1))
+    )
+
+    if number_of_input_variables == 1:
+        cond_nudge = input_dist
+    else:
+        temp_other_inputs = other_input_dist.flatten()
+        number_of_other_input_states = temp_other_inputs.shape[0]
+        temp_inputs = np.reshape(
+            input_dist, (number_of_other_input_states, number_of_nudge_states)
+        )
+        cond_nudge = np.ones(
+            (number_of_other_input_states, number_of_nudge_states)
+        )
+
+        for i in range(temp_other_inputs.flatten().shape[0]):
+            #within this condition the conditional nudge can be every
+            #every distribution so I set it to uniform
+            if temp_other_inputs[i] == 0:
+                cond_nudge[i] = cond_nudge[i]/number_of_nudge_states
+            else:
+                cond_nudge[i] = temp_inputs[i]/temp_other_inputs[i]
+
+        cond_nudge = np.reshape(cond_nudge, input_dist.shape)
+
+    nudge_allignments = []
+    for i in range(number_of_nudge_states):
+        states = [-1]*number_of_nudge_states
+        states[i] = 1
+        nudge_allignments.append(states)
+
+    output_allignments = itertools.product([-1, 1], repeat=number_of_output_states)
+    output_allignments = [allignment for allignment in output_allignments
+                          if allignment != (-1,)*number_of_output_states and 
+                          allignment != (1,)*number_of_output_states]
+    max_impact = 0
+    for nudge_allignment in nudge_allignments:
+        #print("the nudge allignment {}".format(nudge_allignment))
+        for output_allignment in output_allignments:
+            #print("the output allignment is {}".format(output_allignment))
+            allignments = np.array(number_of_input_states*list(output_allignment))
+            allignment_scores = allignments.flatten() * cond_output.flatten()
+            allignment_scores = np.reshape(
+                allignment_scores, 
+                (number_of_input_states, number_of_output_states)
+            )
+            allignment_scores = np.sum(allignment_scores, axis=1)
+            #print("the allignment scores are {}".format(allignment_scores))
+
+            weights = np.repeat(other_input_dist.flatten(), input_dist.shape[-1])
+            #print("the weights are {}".format(weights))
+            weighted_allignment_scores = allignment_scores*weights 
+            #print("weighted allignment scores {}".format(weighted_allignment_scores))
+
+            positive_scores = []
+            negative_states_and_scores = []
+            for state in range(number_of_nudge_states):
+                #select all states from the input distribution for which
+                #the nudged state equals i
+                #print("the variable index is {}".format(i))
+                states = np.take(cond_nudge, indices=state,
+                                 axis=number_of_input_variables-1)
+                states = states.flatten()
+                #print("the just created states are {}".format(states))
+                scores = np.take(
+                    np.reshape(weighted_allignment_scores, input_dist.shape),
+                    indices=state, axis=number_of_input_variables-1
+                )
+                scores = scores.flatten()
+                #print("the states are {}, the scores are {}".format(states, scores))
+
+                if nudge_allignment[state] == 1:
+                    positive_scores = scores
+                else:
+                    negative_states_and_scores.append((states, -1*scores))
+            
+            #print("the positive scores are {}".format(positive_scores))
+            #print("the negative states and scores {}".format(negative_states_and_scores))
+
+            # The the limiting effect of not being able to perform a nudge fully
+            # is already accounted for in the positive nudge
+            routes = create_routes(negative_states_and_scores, nudge_size, positive_scores) 
+            #print("the routes are {}".format(routes))
+            optimal_route, impact_negative_nudge = combining_routes.find_optimal_height_routes(routes, nudge_size)
+            #impact_negative_nudge = combining_routes.find_optimal_height_routes(routes, nudge_size)
+            impact_positive_nudge = nudge_size * sum(positive_scores)
+            
+            #print("the positive impact {}".format(impact_positive_nudge))
+            #print("the negative impact {}".format(impact_negative_nudge))
+
+            if impact_positive_nudge+impact_negative_nudge > max_impact:
+                #print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+                #print("the optimal route is {}".format(optimal_route))
+                #print("the best output allignment is {}".format(output_allignment))
+                #print("the best nudge allignment is {}".format(nudge_allignment))
+                max_impact = impact_positive_nudge+impact_negative_nudge
+            #print("")
+
+    return max_impact
+
 def create_routes(states_and_scores_list, nudge_size, positive_scores):
     routes= []
     for states, scores in states_and_scores_list:
@@ -280,6 +403,8 @@ def create_route(states, scores, nudge_size, positive_scores):
     lower_states_scores = []
     sum_other_scores = 0
     for state, score, pos_score in zip(states, scores, positive_scores):
+        if state == 0:
+            continue
         if state < nudge_size:
             lower_states_scores.append([state, score, pos_score])
         else:
